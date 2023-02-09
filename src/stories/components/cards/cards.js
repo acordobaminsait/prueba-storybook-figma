e931cd33eef5f822896e43ac60d32217021067ab
Alberto Córdoba Rojo <acordoba@minsait.com>
2023-02-09 10:43:47 +0100

    fix card

--- src/stories/components/cards/cards.js
+++ src/stories/components/cards/cards.js
@@ -3,11 +3,22 @@ import './cards.css';
 export const createCard = ({
   size = 'medium',
   backgroundColor,
+  date,
+  title,
   text,
+  link,
 }) => {
 
   const crd = document.createElement('card');
-  crd.innerHTML = "<div id='container' class='storybook-card'>"+text+"</div>";
+  crd.innerHTML = "<div class='storybook-card'>"+
+  "<div class='storybook-card-header'></div>"+
+  "<div class='storybook-card-content'>"+
+    "<div class='storybook-card-text-container'><span class='storybook-card-date'>"+date+"</span>"+
+    "<span class='storybook-card-title'>"+title+"</span>"+
+    "<span class='storybook-card-text'>"+text+"</span></div>"+
+    "<a class='storybook-card-link'>"+link+"</a>"+
+  "</div>"+
+  "</div>";
 
   crd.className = ['storybook-card', `storybook-card--${size}`].join(' ');
 
--- src/stories/components/cards/cards.stories.js
+++ src/stories/components/cards/cards.stories.js
@@ -6,7 +6,10 @@ export default {
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
+    date: { control: 'text' },
+    title: { control: 'text' },
     text: { control: 'text' },
+    link: { control: 'text' },
     size: {
       control: { type: 'select' },
       options: ['small', 'medium', 'large'],
@@ -24,28 +27,40 @@ const Template = ({ text, ...args }) => {
 export const Large = Template.bind({});
 // More on args: https://storybook.js.org/docs/html/writing-stories/args
 Large.args = {
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est nisi, maximus eget est ut, vestibulum varius felis. In eget nibh a felis mollis semper eget ac ligula. Nulla vehicula, dui vitae maximus finibus, metus diam cursus libero, at vehicula odio turpis sed dui. Phasellus eu laoreet risus. Cras nec nisi finibus, auctor lacus quis, mattis neque. In sapien velit, lacinia vel egestas in, gravida eget est. Quisque vitae diam a diam malesuada porta in id libero. Duis lacinia magna at dui commodo ultricies. Aliquam lobortis sit amet mi vitae rhoncus. Nam nulla magna, mollis at ligula aliquet, mattis volutpat turpis. Vivamus placerat scelerisque dolor, a feugiat ligula faucibus id. Maecenas in tristique dolor. Nullam iaculis tellus eu enim aliquet bibendum et sit amet elit. Phasellus at libero in turpis placerat venenatis. Ut convallis tempor commodo. Nulla erat velit, convallis quis feugiat et, iaculis vitae risus. Vestibulum ligula tortor, dapibus vitae diam eu, fermentum lobortis neque. Suspendisse egestas nulla a nisl viverra consectetur. Donec faucibus dapibus dui. Donec egestas tincidunt felis eget bibendum. Ut enim libero, mattis at pretium eu, interdum a sem. Duis nec accumsan massa.',
-  backgroundColor: 'lightgrey',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est nisi, maximus eget est ut, vestibulum varius felis.',
+  backgroundColor: '#F7C92E',
   size: 'large',
+  date: '04/02/2023',
+  title: 'Titular de la noticia',
+  link: 'Leer toda la noticia'
 };
 
 export const Medium = Template.bind({});
 Medium.args = {
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est nisi, maximus eget est ut, vestibulum varius felis. In eget nibh a felis mollis semper eget ac ligula. Nulla vehicula, dui vitae maximus finibus, metus diam cursus libero, at vehicula odio turpis sed dui. Phasellus eu laoreet risus. Cras nec nisi finibus, auctor lacus quis, mattis neque. In sapien velit, lacinia vel egestas in, gravida eget est. Quisque vitae diam a diam malesuada porta in id libero. Duis lacinia magna at dui commodo ultricies. Aliquam lobortis sit amet mi vitae rhoncus. Nam nulla magna, mollis at ligula aliquet, mattis volutpat turpis. Vivamus placerat scelerisque dolor, a feugiat ligula faucibus id. Maecenas in tristique dolor. Nullam iaculis tellus eu enim aliquet bibendum et sit amet elit. Phasellus at libero in turpis placerat venenatis. Ut convallis tempor commodo. Nulla erat velit, convallis quis feugiat et, iaculis vitae risus. Vestibulum ligula tortor, dapibus vitae diam eu, fermentum lobortis neque. Suspendisse egestas nulla a nisl viverra consectetur. Donec faucibus dapibus dui. Donec egestas tincidunt felis eget bibendum. Ut enim libero, mattis at pretium eu, interdum a sem. Duis nec accumsan massa.',
-  backgroundColor: 'lightgrey',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est nisi, maximus eget est ut, vestibulum varius felis.',
+  backgroundColor: '#AC7EF4',
   size: 'medium',
+  date: '04/02/2023',
+  title: 'Titular de la noticia',
+  link: 'Leer toda la noticia'
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est nisi, maximus eget est ut, vestibulum varius felis. In eget nibh a felis mollis semper eget ac ligula. Nulla vehicula, dui vitae maximus finibus, metus diam cursus libero, at vehicula odio turpis sed dui. Phasellus eu laoreet risus. Cras nec nisi finibus, auctor lacus quis, mattis neque. In sapien velit, lacinia vel egestas in, gravida eget est. Quisque vitae diam a diam malesuada porta in id libero. Duis lacinia magna at dui commodo ultricies. Aliquam lobortis sit amet mi vitae rhoncus. Nam nulla magna, mollis at ligula aliquet, mattis volutpat turpis. Vivamus placerat scelerisque dolor, a feugiat ligula faucibus id. Maecenas in tristique dolor. Nullam iaculis tellus eu enim aliquet bibendum et sit amet elit. Phasellus at libero in turpis placerat venenatis. Ut convallis tempor commodo. Nulla erat velit, convallis quis feugiat et, iaculis vitae risus. Vestibulum ligula tortor, dapibus vitae diam eu, fermentum lobortis neque. Suspendisse egestas nulla a nisl viverra consectetur. Donec faucibus dapibus dui. Donec egestas tincidunt felis eget bibendum. Ut enim libero, mattis at pretium eu, interdum a sem. Duis nec accumsan massa.',
-  backgroundColor: 'lightgrey',
-  size: 'small',
-};
+// export const Small = Template.bind({});
+// Small.args = {
+//   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est nisi, maximus eget est ut, vestibulum varius felis. In eget nibh a felis mollis semper eget ac ligula. Nulla vehicula, dui vitae maximus finibus, metus diam cursus libero, at vehicula odio turpis sed dui.',
+//   backgroundColor: 'lightgrey',
+//   size: 'small',
+// };
 
 Large.parameters = {
   design: {
     type: "figma",
-    url: "https://www.figma.com/file/d91rGVVI3tNAdsJk9QQoJ1/storybook-con-figma?node-id=73%3A4298&t=UFBL2mSDeQvR6i4J-0",
+    url: "https://www.figma.com/file/d91rGVVI3tNAdsJk9QQoJ1/storybook-con-figma?node-id=114%3A4648&t=c9WOxucjKTlXFCTi-0",
+  },
+};
+Medium.parameters = {
+  design: {
+    type: "figma",
+    url: "https://www.figma.com/file/d91rGVVI3tNAdsJk9QQoJ1/storybook-con-figma?node-id=73%3A4528&t=3DzAWzrFQrQfalUs-0",
   },
 };
